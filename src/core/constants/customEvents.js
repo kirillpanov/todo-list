f9e4ecb9ea33f2e4f9ff984dc3cb34ff0f320f39
Panou, Kiryl <Kiryl_Panou@epam.com>
2019-08-07 16:00:20 +0300

    feat: build

--- src/core/constants/customEvents.js
+++
@@ -1,3 +0,0 @@
-export const CUSTOM_EVENTS = {
-    TOGGLE_FULFILL_TODO: "toggleFulfillTodo"
-};
--- src/core/utils/getFormTemplate.util.js
+++
@@ -1,7 +0,0 @@
-export function getFormTemplate() {
-    return `
-        <label for='todo'>ToDo:</label>
-        <input type='text' name='todo' autocomplete="off" required>
-        <input class='submit-button' type='submit' value='Add todo' disabled>
-    `;
-}
